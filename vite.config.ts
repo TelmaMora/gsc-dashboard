10c864559d86b410004e6e0a3c599ddcba41849a
Telma Mora <macbookpro@MacBook-Pro-de-Macbook.local>
2025-11-13 13:54:51 -0600

    Preparar app para deploy en Render

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: './', // <- muy importante para deploy en Render
 })
